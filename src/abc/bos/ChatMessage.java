2bfcad82e8453bada308f8e5180434fcdf93f967
r <r@pc.x>
2022-03-12 15:20:24 +0200

    ChatMessage.toString()

--- src/abc/bos/ChatMessage.java
+++ src/abc/bos/ChatMessage.java
@@ -3,17 +3,36 @@ package abc.bos;
 import java.io.Serializable;
 
 
-public class ChatMessage implements Serializable {
+public class ChatMessage /* implements Serializable */ {
   
   //-------------------------------------------------------------------------//
   //  Attributes                                                             //
   //-------------------------------------------------------------------------//
-  public java.util.Date  timeStamp ;
-  public String          id        ;
-  public String          from      ;
-  public String          to        ;
-  public String          body      ;
-  public boolean         shown     ;
+  public java.util.Date  timeStamp   ;
+  public String          id          ;
+  public String          sender      ;
+  public String          destination ;
+  public String          body        ;
+  public boolean         shown       ;
+  
+  
+  //-------------------------------------------------------------------------//
+  //  toString()                                                             //
+  //-------------------------------------------------------------------------//
+  @Override
+  public String toString() {
+    
+    String s = "\r\n"
+      + "\r\n"
+      + "timeStamp"   + " = " + timeStamp   + "\r\n"
+      + "id"          + " = " + id          + "\r\n"
+      + "sender"      + " = " + sender      + "\r\n"
+      + "destination" + " = " + destination + "\r\n"
+      + "body"        + " = " + body        + "\r\n"
+      + "shown"       + " = " + shown       + "\r\n";
+    
+    return s;
+  } //toString()
   
   
 } //class
