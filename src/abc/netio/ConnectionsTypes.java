5824bb299ae35c67023127760b5cd6c24852a860
r <r@pc.x>
2022-03-12 20:10:15 +0200

    Connection types.

---
+++ src/abc/netio/ConnectionsTypes.java
@@ -0,0 +1,13 @@
+package abc.netio;
+
+
+public class ConnectionsTypes {
+  
+  //-------------------------------------------------------------------------//
+  //  Constants                                                              //
+  //-------------------------------------------------------------------------//
+  public static final String ACTIVE_CLIENT  = "ACTIVE_CLIENT"  ;
+  public static final String PASSIVE_CLIENT = "PASSIVE_CLIENT" ;
+  
+  
+} //class
