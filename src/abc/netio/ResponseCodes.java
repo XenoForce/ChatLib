819767a378620f44ba951a0ac0e7f886dcb990e6
r <r@pc.x>
2022-03-13 00:56:24 +0200

    Response Codes

---
+++ src/abc/netio/ResponseCodes.java
@@ -0,0 +1,13 @@
+package abc.netio;
+
+
+public class ResponseCodes {
+  
+  //-------------------------------------------------------------------------//
+  //  Constants                                                              //
+  //-------------------------------------------------------------------------//
+  public static final String  OK    = "OK"    ;
+  public static final String  ERROR = "ERROR" ;
+  
+  
+} //class
