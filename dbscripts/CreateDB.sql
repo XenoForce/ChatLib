1e1cded4909afe03ed57a4f9ee54a5fc44e7c1a4
r <r@pc.x>
2022-03-12 15:31:16 +0200

    Create db sql script

---
+++ dbscripts/CreateDB.sql
@@ -0,0 +1,8 @@
+create table ChatMessage (
+  timeStamp    timestamp     not null ,
+  id           varchar(50)   not null ,
+  sender       varchar(50)   not null ,
+  destination  varchar(50)   not null ,
+  body         varchar(5000) not null ,
+  shown        boolean       not null
+)
