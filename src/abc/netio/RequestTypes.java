31f74dc0b9526f47c6b25c579d88dfbfb8b8c976
r <r@pc.x>
2022-03-18 12:45:32 +0200

    constants: RequestTypes

---
+++ src/abc/netio/RequestTypes.java
@@ -0,0 +1,14 @@
+package abc.netio;
+
+
+public class RequestTypes {
+  
+  //-------------------------------------------------------------------------//
+  //  Constants                                                              //
+  //-------------------------------------------------------------------------//
+  public static final String  GET_MSGS_SINCE_CUTOFF = "GET_MSGS_SINCE_CUTOFF" ;
+  public static final String  PROCESS_ONE_NEW_MSG   = "PROCESS_ONE_NEW_MSG"   ;
+  public static final String  UPDATE_SHOWN_STATUS   = "UPDATE_SHOWN_STATUS"   ;
+  
+  
+} //class
